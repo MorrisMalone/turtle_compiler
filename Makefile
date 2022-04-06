4811b29a408cf811665d7ddea0cdbe29d349c0e5
johan morin <johan.morin@hotmail.fr>
2022-04-06 11:42:37 +0200

    add makefile, can compile

---
+++ Makefile
@@ -0,0 +1,2 @@
+lexer:
+	gcc -lSDL2 $(sdl2-config --cflags) -Wall -lm   -o lexer lexer.c turtle-eval.c turtle-main.c turtle-nametab.c sdlinterf.c $(sdl2-config --libs) 
--- turtle-main.c
+++ turtle-main.c
@@ -44,29 +44,29 @@ void code_error(const srcpos_t *pos, const char *format, ...)
   exit(EXIT_FAILURE);
 }
 
-int main(int argc, const char *argv[])
-{
-  if (argc < 2) {
-    fprintf(stderr, "Aufruf: %s Programm-Datei [Zahlen ...]\n", argv[0]);
-    exit(EXIT_FAILURE);
-  }
-  if (argc > 11) {
-    fprintf(stderr, "Aufruf: %s Programm-Datei [Zahlen ...]\n"
-                    "Höchstens 9 Zahlen!\n",
-            argv[0]);
-    exit(EXIT_FAILURE);
-  }
+// int main(int argc, const char *argv[])
+// {
+//   if (argc < 2) {
+//     fprintf(stderr, "Aufruf: %s Programm-Datei [Zahlen ...]\n", argv[0]);
+//     exit(EXIT_FAILURE);
+//   }
+//   if (argc > 11) {
+//     fprintf(stderr, "Aufruf: %s Programm-Datei [Zahlen ...]\n"
+//                     "Höchstens 9 Zahlen!\n",
+//             argv[0]);
+//     exit(EXIT_FAILURE);
+//   }
   
-  src_file = fopen(argv[1], "r");
-  if (src_file == NULL) {
-    fprintf(stderr, "%s: Fehler beim Öffnen von %s zum Lesen: %s\n",
-            argv[0], argv[1], strerror(errno));
-    exit(EXIT_FAILURE);
-  }
+//   src_file = fopen(argv[1], "r");
+//   if (src_file == NULL) {
+//     fprintf(stderr, "%s: Fehler beim Öffnen von %s zum Lesen: %s\n",
+//             argv[0], argv[1], strerror(errno));
+//     exit(EXIT_FAILURE);
+//   }
 
-  prog_name = argv[0];
+//   prog_name = argv[0];
 
-  evaluate(parse(), argc - 2, &(argv[2]));
+//   // evaluate(parse(), argc - 2, &(argv[2]));
 
-  exit(EXIT_SUCCESS);
-}
+//   exit(EXIT_SUCCESS);
+// }
